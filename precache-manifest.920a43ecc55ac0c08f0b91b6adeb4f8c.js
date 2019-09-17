6807c2f3a5452a78bf4ee10b1cb4e2a3d053ffb5
MarMMcin <em-erce@wp.pl>
2019-09-17 04:09:07 +0200

    Updates

---
+++ precache-manifest.920a43ecc55ac0c08f0b91b6adeb4f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b9e6a874642fd30c37a8e109f01c6e",
+    "url": "/MarMMcin/estate.git/index.html"
+  },
+  {
+    "revision": "43cc6321ebedb10ac83a",
+    "url": "/MarMMcin/estate.git/static/css/main.27e77c78.chunk.css"
+  },
+  {
+    "revision": "8a8984728c01daa5b7f8",
+    "url": "/MarMMcin/estate.git/static/js/2.34e87a3b.chunk.js"
+  },
+  {
+    "revision": "43cc6321ebedb10ac83a",
+    "url": "/MarMMcin/estate.git/static/js/main.818d946a.chunk.js"
+  },
+  {
+    "revision": "0f16c52fa67fdc2a2034",
+    "url": "/MarMMcin/estate.git/static/js/runtime~main.5e6efa3a.js"
+  },
+  {
+    "revision": "621aa230cae1da8cb7abc3fbe71a779c",
+    "url": "/MarMMcin/estate.git/static/media/logo2.621aa230.jpg"
+  }
+]);
